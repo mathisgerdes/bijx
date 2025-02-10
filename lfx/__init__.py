207485955588e873c07bad95c155d7d195cf5b0a
Mathis Gerdes <MathisGerdes@gmail.com>
2025-02-10 10:46:25 +0100

    Add continuous flow example to tutorial.

--- lfx/__init__.py
+++ lfx/__init__.py
@@ -1,5 +1,7 @@
+from ._version import __version__
 from .bijections import (Bijection, Chain, Const, Frozen, Inverse, Scaling,
                          Shift, filter_frozen)
 from .prior import (ArrayPrior, IndependentNormal, IndependentUniform, Prior,
                     Sampler)
 from .sym_conv import SymConv
+from .utils import effective_sample_size, moving_average
--- lfx/utils.py
+++ lfx/utils.py
@@ -1,1 +1,34 @@
+from functools import partial
+
 import jax
+import jax.numpy as jnp
+
+
+@jax.jit
+def effective_sample_size(logp: jnp.ndarray, logq: jnp.ndarray) -> jnp.ndarray:
+    """Compute the ESS given log likelihoods.
+
+    The two likelihood arrays must be evaluated for the same set of samples.
+    The samples are assumed to be drawn from ``q``, such that ``logq``
+    are the corresponding log-likelihoods, and ``p`` is the target.
+
+    Args:
+        logp: The log likelihood of p (up to a constant shift).
+        logq: The log likelihood of q (up to a constant shift).
+
+    Returns:
+        The effective sample size per sample (between 0 and 1).
+    """
+    logw = logp - logq
+    log_ess = 2*jax.nn.logsumexp(logw, axis=0) - jax.nn.logsumexp(2*logw, axis=0)
+    ess_per_sample = jnp.exp(log_ess) / len(logw)
+    return ess_per_sample
+
+
+@partial(jax.jit, static_argnums=(1,))
+def moving_average(x: jnp.ndarray, window: int = 10):
+    """Moving average over 1d array."""
+    if len(x) < window:
+        return jnp.mean(x, keepdims=True)
+    else:
+        return jnp.convolve(x, jnp.ones(window), 'valid') / window
